994c3c5f127292b25a156619b35dc3330f629604
Gunharth Randolf <hello@gunharth.io>
2018-12-27 22:36:36 +0100

    Add web server and npm global server start

--- api/server.js
+++ api/server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('<h1>Calculator Backend<h2>'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

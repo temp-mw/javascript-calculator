92c7e53baf55f8c887ef4598ca971aa5504a61b9
Gunharth Randolf <hello@gunharth.io>
2018-12-28 14:03:19 +0100

    Add first key bindings

--- web/src/app.js
+++ web/src/app.js
@@ -1,1 +1,42 @@
-console.log('working');
+'use strict';
+
+/*
+Define the calculator parts (calulator, display and keys)
+*/
+const calculator = document.getElementById('calculator');
+const display = document.querySelector('.display');
+const keys = calculator.querySelector('.keys');
+
+/* Add the click event listener for all keys ie buttons*/
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action; // if exists get the data-action attribute from the button
+
+        if (!action) {
+            console.log('number key!');
+        }
+
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            console.log('operator key!');
+        }
+
+        if (action === 'decimal') {
+            console.log('decimal key!');
+        }
+
+        if (action === 'clear') {
+            console.log('clear!');
+        }
+
+        if (action === 'calculate') {
+            console.log('equal key!');
+        }
+
+    }
+});

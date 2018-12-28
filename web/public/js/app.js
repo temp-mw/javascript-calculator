60ccb0f498c7cfb2f82e9aee37138313a7388399
Gunharth Randolf <hello@gunharth.io>
2018-12-28 22:10:12 +0100

    Add calculator operations

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -12,14 +12,32 @@
 
 window.$ = window.jQuery = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 /*
+To be performed by nodejs via ajax/json
+*/
+
+var calculate = function calculate(n1, operator, n2) {
+  var firstNum = parseFloat(n1);
+  var secondNum = parseFloat(n2); // let's not use eval(firstNum + secondNum)
+
+  if (operator === 'add') return precision(firstNum + secondNum);
+  if (operator === 'subtract') return precision(firstNum - secondNum);
+  if (operator === 'multiply') return precision(firstNum * secondNum);
+  if (operator === 'divide') return precision(firstNum / secondNum);
+};
+
+var precision = function precision(result) {
+  return Math.round(1e12 * result) / 1e12;
+};
+/*
 Define the calculator parts (calulator, display and keys)
 */
+
 
 var calculator = $('#calculator');
 var display = $('#calculator .display');
 var keys = $('#calculator .keys');
-/* Add the click event listener for all keys ie buttons*/
-// keys.addEventListener('click', e => {
+var clearButton = keys.find('button[data-action=clear]');
+/* Add the click event listener for all keys */
 
 keys.on('click', function (e) {
   if (e.target.matches('button')) {
@@ -27,24 +45,100 @@ keys.on('click', function (e) {
 
     var action = key.attr('data-action'); // if exists get the data-action attribute from the button
 
+    var keyContent = key.text(); // value of the pressed key
+
+    var displayedNum = display.text(); // value of the display
+
+    var previousKeyType = calculator.data('previousKeyType'); // memorize the previous key type
+    // Remove .is-pressed class from all keys
+
+    keys.find('button').removeClass('is-pressed');
+
     if (!action) {
-      console.log('number key!');
+      // number keys
+      if (displayedNum === '0' || previousKeyType === 'operator' || previousKeyType === 'calculate') {
+        display.text(keyContent);
+      } else {
+        display.text(displayedNum + keyContent);
+      }
+
+      calculator.data('previousKeyType', 'number');
     }
 
-    if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
-      console.log('operator key!');
+    if ( // operators
+    action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+      var firstValue = calculator.data('firstValue');
+      var operator = calculator.data('operator');
+      var secondValue = displayedNum; // Note: It's sufficient to check for firstValue and operator because secondValue always exists
+
+      if (firstValue && operator && previousKeyType !== 'operator' && previousKeyType !== 'calculate') {
+        var calcValue = calculate(firstValue, operator, secondValue);
+        display.text(calcValue); // Update firstValue to calculated value
+
+        calculator.data('firstValue', calcValue);
+      } else {
+        // If there are no calculations, set displayedNum as the firstValue
+        calculator.data('firstValue', displayedNum);
+      }
+
+      key.addClass('is-pressed'); // used to style operators
+
+      calculator.data('previousKeyType', 'operator');
+      calculator.data('operator', action);
     }
 
     if (action === 'decimal') {
-      console.log('decimal key!');
+      // Do nothing if string has a dot
+      if (!displayedNum.includes('.')) {
+        display.text(displayedNum + '.');
+      } else if (previousKeyType === 'operator' || previousKeyType === 'calculate') {
+        display.text('0.');
+      }
+
+      calculator.data('previousKeyType', 'decimal');
     }
 
     if (action === 'clear') {
-      console.log('clear!');
+      if (key.text() === 'AC') {
+        calculator.data('firstValue', '');
+        calculator.data('modValue', '');
+        calculator.data('operator', '');
+        calculator.data('previousKeyType', '');
+      } else {
+        key.text('AC');
+        calculator.data('firstValue', '');
+        calculator.data('modValue', '');
+        calculator.data('operator', '');
+        calculator.data('previousKeyType', '');
+      }
+
+      display.text(0);
+      calculator.data('previousKeyType', 'clear');
     }
 
     if (action === 'calculate') {
-      console.log('equal key!');
+      var _firstValue = calculator.data('firstValue');
+
+      var _operator = calculator.data('operator');
+
+      var _secondValue = displayedNum;
+
+      if (_firstValue) {
+        if (previousKeyType === 'calculate') {
+          _firstValue = displayedNum;
+          _secondValue = calculator.data('modValue');
+        }
+
+        display.text(calculate(_firstValue, _operator, _secondValue));
+      } // Set modValue attribute
+
+
+      calculator.data('modValue', _secondValue);
+      calculator.data('previousKeyType', 'calculate');
+    }
+
+    if (action !== 'clear') {
+      clearButton.text('CE');
     }
   }
 });
--- web/src/app.js
+++ web/src/app.js
@@ -3,42 +3,141 @@
 window.$ = window.jQuery = require('jquery');
 
 /*
+To be performed by nodejs via ajax/json
+*/
+const calculate = (n1, operator, n2) => {
+    const firstNum = parseFloat(n1);
+    const secondNum = parseFloat(n2);
+    // let's not use eval(firstNum + secondNum)
+    if (operator === 'add') return precision(firstNum + secondNum);
+    if (operator === 'subtract') return precision(firstNum - secondNum);
+    if (operator === 'multiply') return precision(firstNum * secondNum);
+    if (operator === 'divide') return precision(firstNum / secondNum);
+};
+
+const precision = (result) => {
+    return Math.round(1e12 * result) / 1e12;
+};
+
+/*
 Define the calculator parts (calulator, display and keys)
 */
 const calculator = $('#calculator');
 const display = $('#calculator .display');
 const keys = $('#calculator .keys');
+const clearButton = keys.find('button[data-action=clear]');
 
-/* Add the click event listener for all keys ie buttons*/
-// keys.addEventListener('click', e => {
+
+/* Add the click event listener for all keys */
 keys.on('click', e => {
     if (e.target.matches('button')) {
         const key = $(e.target); // make target a jQuery object
         const action = key.attr('data-action'); // if exists get the data-action attribute from the button
+        const keyContent = key.text(); // value of the pressed key
+        const displayedNum = display.text(); // value of the display
+        const previousKeyType = calculator.data('previousKeyType'); // memorize the previous key type
 
-        if (!action) {
-            console.log('number key!');
+        // Remove .is-pressed class from all keys
+        keys.find('button').removeClass('is-pressed');
+
+        if (!action) { // number keys
+            if (
+                displayedNum === '0' ||
+                previousKeyType === 'operator' ||
+                previousKeyType === 'calculate'
+            ) {
+                display.text(keyContent);
+            } else {
+                display.text(displayedNum + keyContent);
+            }
+            calculator.data('previousKeyType', 'number');
         }
 
-        if (
+        if ( // operators
             action === 'add' ||
             action === 'subtract' ||
             action === 'multiply' ||
             action === 'divide'
         ) {
-            console.log('operator key!');
+            const firstValue = calculator.data('firstValue');
+            const operator = calculator.data('operator');
+            const secondValue = displayedNum;
+
+            // Note: It's sufficient to check for firstValue and operator because secondValue always exists
+            if (
+                firstValue &&
+                operator &&
+                previousKeyType !== 'operator' &&
+                previousKeyType !== 'calculate'
+            ) {
+                const calcValue = calculate(firstValue, operator, secondValue);
+                display.text(calcValue);
+
+                // Update firstValue to calculated value
+                calculator.data('firstValue', calcValue);
+            } else {
+                // If there are no calculations, set displayedNum as the firstValue
+                calculator.data('firstValue', displayedNum);
+            }
+
+            key.addClass('is-pressed'); // used to style operators
+
+            calculator.data('previousKeyType', 'operator');
+            calculator.data('operator', action);
         }
 
         if (action === 'decimal') {
-            console.log('decimal key!');
+            // Do nothing if string has a dot
+            if (!displayedNum.includes('.')) {
+                display.text(displayedNum + '.');
+            } else if (
+                previousKeyType === 'operator' ||
+                previousKeyType === 'calculate'
+            ) {
+                display.text('0.');
+            }
+
+            calculator.data('previousKeyType', 'decimal');
         }
 
         if (action === 'clear') {
-            console.log('clear!');
+            if (key.text() === 'AC') {
+                calculator.data('firstValue', '');
+                calculator.data('modValue', '');
+                calculator.data('operator', '');
+                calculator.data('previousKeyType', '');
+            } else {
+                key.text('AC');
+                calculator.data('firstValue', '');
+                calculator.data('modValue', '');
+                calculator.data('operator', '');
+                calculator.data('previousKeyType', '');
+            }
+
+            display.text(0);
+            calculator.data('previousKeyType', 'clear');
         }
 
         if (action === 'calculate') {
-            console.log('equal key!');
+            let firstValue = calculator.data('firstValue');
+            const operator = calculator.data('operator');
+            let secondValue = displayedNum;
+
+            if (firstValue) {
+                if (previousKeyType === 'calculate') {
+                    firstValue = displayedNum;
+                    secondValue = calculator.data('modValue');
+                }
+
+                display.text(calculate(firstValue, operator, secondValue));
+            }
+            // Set modValue attribute
+            calculator.data('modValue', secondValue);
+            calculator.data('previousKeyType', 'calculate');
+        }
+
+        if (action !== 'clear') {
+            clearButton.text('CE');
         }
 
     }

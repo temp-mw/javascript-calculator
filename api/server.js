c7a8ce31075cd0c5ac5bd0b5ec673fc9a2afe630
Gunharth Randolf <hello@gunharth.io>
2018-12-31 16:29:12 +0100

    wip save results to file with node

--- api/server.js
+++ api/server.js
@@ -1,5 +1,7 @@
-const Decimal = require('decimal.js');
 const express = require('express');
+const Decimal = require('decimal.js');
+const fs = require('fs');
+
 const app = express();
 const port = 3001;
 
@@ -13,40 +15,63 @@ app.use(function (req, res, next) { // middleware for CORS headers
     next();
 });
 
-const calculate = (n1, operator, n2 = 0) => {
-    const firstNum = new Decimal(n1);
-    const secondNum = new Decimal(n2);
+/* calculates the operations
+uses decimal.js and
+the callback nodeActions to work with the results
+*/
+const calculate = (n1, operator, n2 = 0, callback) => {
+    n1 = new Decimal(n1);
+    n2 = new Decimal(n2);
 
     // operators
-    // if (operator === 'add') return { result: precision(firstNum + secondNum), calc: `${firstNum} + ${secondNum}` };
-    if (operator === 'add') return { result: firstNum.plus(secondNum).toNumber(), calc: `${firstNum} + ${secondNum}` };
-    // if (operator === 'subtract') return { result: precision(firstNum - secondNum), calc: `${firstNum} - ${secondNum}` };
-    if (operator === 'subtract') return { result: firstNum.sub(secondNum).toNumber(), calc: `${firstNum} - ${secondNum}` };
-    // if (operator === 'multiply') return { result: precision(firstNum * secondNum), calc: `${firstNum} * ${secondNum}` };
-    if (operator === 'multiply') return { result: firstNum.mul(secondNum).toNumber(), calc: `${firstNum} * ${secondNum}` };
-    // if (operator === 'divide') return { result: precision(firstNum / secondNum), calc: `${firstNum} / ${secondNum}` };
-    if (operator === 'divide') return { result: firstNum.div(secondNum).toNumber(), calc: `${firstNum} / ${secondNum}` };
-    // power of
-    // if (operator === 'pow') return { result: precision(Math.pow(firstNum, 2)), calc: `Math.pow(${firstNum},2)` };
-    if (operator === 'pow') return { result: firstNum.toPower(2).toNumber(), calc: `pow(${firstNum},2)` };
-    if (operator === 'pow3') return { result: firstNum.toPower(3).toNumber(), calc: `pow(${firstNum},3)` };
-    if (operator === 'powy') return { result: firstNum.toPower(secondNum).toNumber(), calc: `pow(${firstNum},${secondNum})` };
-    // if (operator === 'pow3') return { result: precision(Math.pow(firstNum, 3)), calc: `Math.pow(${firstNum},3)` };
-    // if (operator === 'powy') return { result: precision(Math.pow(firstNum, secondNum)), calc: `Math.pow(${firstNum},${secondNum})` };
-    // square root
-    // if (operator === 'sqrt') return { result: precision(Math.sqrt(firstNum)), calc: `Math.sqrt(${firstNum})` };
-    if (operator === 'sqrt') return { result: firstNum.squareRoot().toNumber(), calc: `sqrt(${firstNum})` };
-    // if (operator === 'cbrt') return { result: precision(Math.cbrt(firstNum)), calc: `Math.cbrt(${firstNum})` };
-    if (operator === 'cbrt') return { result: firstNum.cbrt().toNumber(), calc: `cbrt(${firstNum})` };
-    // if (operator === 'nthrt') return { result: precision(Math.pow(firstNum, 1 / secondNum)), calc: `Math.pow(${firstNum}, 1 / ${secondNum})` };
-    if (operator === 'nthrt') return { result: firstNum.pow(new Decimal(1).div(secondNum)).toNumber(), calc: `pow(${firstNum}, 1 / ${secondNum})` };
+    // if (operator === 'add') return { result: precision(n1 + n2), calc: `${n1} + ${n2}` };
+    if (operator === 'add') return callback({ result: n1.plus(n2).toNumber(), calc: `${n1} + ${n2}` });
+    // if (operator === 'subtract') return { result: precision(n1 - n2), calc: `${n1} - ${n2}` };
+    if (operator === 'subtract') return callback({ result: n1.sub(n2).toNumber(), calc: `${n1} - ${n2}` });
+    // // if (operator === 'multiply') return { result: precision(n1 * n2), calc: `${n1} * ${n2}` };
+    if (operator === 'multiply') return callback({ result: n1.mul(n2).toNumber(), calc: `${n1} * ${n2}` });
+    // // if (operator === 'divide') return { result: precision(n1 / n2), calc: `${n1} / ${n2}` };
+    if (operator === 'divide') return callback({ result: n1.div(n2).toNumber(), calc: `${n1} / ${n2}` });
+    // // power of
+    // // if (operator === 'pow') return { result: precision(Math.pow(n1, 2)), calc: `Math.pow(${n1},2)` };
+    if (operator === 'pow') return callback({ result: n1.toPower(2).toNumber(), calc: `pow(${n1},2)` });
+    if (operator === 'pow3') return callback({ result: n1.toPower(3).toNumber(), calc: `pow(${n1},3)` });
+    if (operator === 'powy') return callback({ result: n1.toPower(n2).toNumber(), calc: `pow(${n1},${n2})` });
+    // // if (operator === 'pow3') return { result: precision(Math.pow(n1, 3)), calc: `Math.pow(${n1},3)` };
+    // // if (operator === 'powy') return { result: precision(Math.pow(n1, n2)), calc: `Math.pow(${n1},${n2})` };
+    // // square root
+    // // if (operator === 'sqrt') return { result: precision(Math.sqrt(n1)), calc: `Math.sqrt(${n1})` };
+    if (operator === 'sqrt') return callback({ result: n1.squareRoot().toNumber(), calc: `sqrt(${n1})` });
+    // // if (operator === 'cbrt') return { result: precision(Math.cbrt(n1)), calc: `Math.cbrt(${n1})` };
+    if (operator === 'cbrt') return callback({ result: n1.cbrt().toNumber(), calc: `cbrt(${n1})` });
+    // // if (operator === 'nthrt') return { result: precision(Math.pow(n1, 1 / n2)), calc: `Math.pow(${n1}, 1 / ${n2})` };
+    if (operator === 'nthrt') return callback({ result: n1.pow(new Decimal(1).div(n2)).toNumber(), calc: `pow(${n1}, 1 / ${n2})` });
+
 };
+
+
+
+function nodeActions(data) {
+    // const file = fs.readFileSync('calculations.txt');
+    // const fd = fs.openSync('calculations.txt', 'w+');
+    // const insert = new Buffer(`${data.calc} = ${data.result}\n`);
+    // fs.writeSync(fd, insert, 0, insert.length, 0);
+    // fs.writeSync(fd, file, 0, file.length, insert.length);
+    // fs.close(fd, (err) => {
+    //     if (err) throw err;
+    // });
+    // fs.appendFile('calculations.txt', `${data.calc} = ${data.result}\n`, function (err) {
+    //     if (err) throw err;
+    //     console.log('Saved!');
+    // });
+    return data;
+}
 
 // const precision = (result) => {
 //     return Math.round(1e12 * result) / 1e12;
 // };
 
-app.post('/', (req, res) => res.json(calculate(req.body.n1, req.body.operator, req.body.n2)));
+app.post('/', (req, res) => res.json(calculate(req.body.n1, req.body.operator, req.body.n2, nodeActions)));
 
 app.get('/', (req, res) => res.send('<h1>Calculator Backend<h2>'));
 

28c408ffbd5423dbe14a528bf54f90ffbffe6a8e
Gunharth Randolf <hello@gunharth.io>
2018-12-31 15:16:01 +0100

    Add desc for cors headers

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const port = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(function (req, res, next) { // add headers for CORS
+app.use(function (req, res, next) { // middleware for CORS headers
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');

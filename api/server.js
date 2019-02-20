4a20b9b80ab2be922bbb5ad8875b62a29bcb8b6a
Gunharth Randolf <hello@gunharth.io>
2019-02-20 16:41:16 +0100

    Code cleanups

--- api/server.js
+++ api/server.js
@@ -9,9 +9,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(function (req, res, next) { // middleware for CORS headers
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Credentials', true);
     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 

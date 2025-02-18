465c0d9b582948e867829cf1b88fcb00d19ef02e
Sanjay Suthar <sanjaymsuthar@gmail.com>
2025-02-18 15:41:37 +0530

    minor 4

--- instrumentation.js
+++ instrumentation.js
@@ -2,7 +2,7 @@ const tracker = require('@middleware.io/node-apm');
 tracker.track({
     serviceName: "js-calculator1",
     accessToken: "snaczyvheqconfmojfldnxfnbtxynkhxxkpy",
-    // target: "https://wbnkg_ekd.mw.lc",
+    //target: "https://wbnkg_ekd.mw.lc",
     target: "https://4569-103-156-143-126.ngrok-free.app",
     customResourceAttributes: {
         git_commit_sha: "ac4cb8d2c54640fabe039b83433a8484609c829c",
